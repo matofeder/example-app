a0f86906df87894d0c3c76a9920f5f4b4dda6180
Matej Feder <matej.feder@dnation.cloud>
2025-08-03 14:16:03 +0200

    Init
    Signed-off-by: Matej Feder <feder.mato@gmail.com>

---
+++ app/page.tsx
@@ -0,0 +1,20 @@
+export default function Home() {
+  const heading = "Welcome to the Jungle";
+  return (
+    <main
+      className="min-h-screen flex flex-col items-center justify-center font-sans bg-cover bg-center"
+      style={{
+        backgroundImage: `url('/jungle.png')`
+      }}
+    >
+      <div className="bg-black/60 p-8 rounded-lg">
+        <h1 className="text-4xl md:text-6xl font-bold mb-4 text-white text-center drop-shadow-lg">
+          {heading}
+        </h1>
+        <p className="text-2xl text-white-700 text-center">
+          This is a very basic example page :)
+        </p>
+      </div>
+    </main>
+  );
+}
